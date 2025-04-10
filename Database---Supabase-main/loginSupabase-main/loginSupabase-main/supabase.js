a14a3dffb1900c69b1aabfe1d4c40f25c507b2f4
Julindoti <julinmestredoti@gmail.com>
2025-04-10 17:30:07 -0300

    Delete Database---Supabase-main/loginSupabase-main/loginSupabase-main directory

--- Database---Supabase-main/loginSupabase-main/loginSupabase-main/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabase = createClient(
-    "https://nzvzwpffucfzyliezfef.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56dnp3cGZmdWNmenlsaWV6ZmVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5OTA0MjksImV4cCI6MjA1ODU2NjQyOX0.O5SJfOmzOKxzITvMcn_3lrGvuD1m_VIHnmQZe5d2vmo");
-
-export {supabase}
